8f165ce41bfb55f3898037ac0830e0d870992054
niv ben-porath <nbpapps@gmail.com>
2020-02-11 14:01:38 +0200

    first commit

---
+++ Sources/SPM_Demo/SPM_Demo.swift
@@ -0,0 +1,11 @@
+public struct SPM_Demo {
+    var text = "Hello, World!"
+    
+    public init(){}
+    
+    var text = "Hello, World!"
+    
+    public func getText() -> String {
+        return text
+    }
+}
